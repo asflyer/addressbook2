1bbb34493fa106c093308b6b26ca1962eba4b6bf
Alistan <nikitinas-93@mail.ru>
2019-06-16 01:03:22 +0300

    Лекция 6.4

--- addressbook-test-data-generators/Program.cs
+++ addressbook-test-data-generators/Program.cs
@@ -7,24 +7,26 @@ using System.Threading.Tasks;
 using web_addressbook_test;
 using System.Xml;
 using System.Xml.Serialization;
-
+using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace addressbook_test_data_generators
 {
     //Запускаемый exeшник в D:\c#projects\addressbook2\addressbook2\addressbook-test-data-generators\bin\Debug\addressbook-test-data-generators.exe
     //Для запуска копировать в cmd
 
-    //D:\c#projects\addressbook2\addressbook2\addressbook-test-data-generators\bin\Debug\addressbook-test-data-generators.exe 2 groups.csv
-    //тут 2 - кол-во строк, groups.csv - имя файла
-
-    //D:\c#projects\addressbook2\addressbook2\addressbook-test-data-generators\bin\Debug\addressbook-test-data-generators.exe 2 groups.xml xml
+    //D:\c#projects\addressbook2\addressbook2\addressbook-test-data-generators\bin\Debug\addressbook-test-data-generators.exe 2 groups.csv csv
+    //тут 2 - кол-во строк, groups.csv - имя файла, csv - расширение
+    //D:\c#projects\addressbook2\addressbook2\addressbook-test-data-generators\bin\Debug\addressbook-test-data-generators.exe 2 groups.xlsx excel
+    //D:\c#projects\addressbook2\addressbook2\addressbook-test-data-generators\bin\Debug\addressbook-test-data-generators.exe 3 groups.json json
     class Program
     {
         static void Main(string[] args)
         {
-            int count = Convert.ToInt32(args[0]); //Кол-во генерируемых ТД
-            StreamWriter writer = new StreamWriter(args[1]);//Записываем в файл с именем args[1]
+            
 
+            int count = Convert.ToInt32(args[0]); //Кол-во генерируемых ТД
+            string filename = args[1];
             string format = args[2];//было3
 
             List<GroupData> groups = new List<GroupData>();
@@ -37,23 +39,62 @@ namespace addressbook_test_data_generators
                 });
 
             }
-           
-            //System.Console.Out.Write("Hello, World");
-            //Файл генерируется в C:\Users\Александр
-            if (format == "csv")
+
+            if (format == "excel")
             {
-                WriteGroupsToCsvFile(groups, writer);
-            }
-            else if (format == "xml")
-            {
-                WriteGroupsToXmlFile(groups, writer);
+                WriteGroupsToExcelFile(groups, filename);
             }
             else
             {
-                System.Console.Out.Write("Unrecognized format" + format);
+                StreamWriter writer = new StreamWriter(filename);//Записываем в файл с именем args[1]
+                if (format == "csv")
+                {
+                    WriteGroupsToCsvFile(groups, writer);
+                }
+                else if (format == "xml")
+                {
+                    WriteGroupsToXmlFile(groups, writer);
+                }
+                else if (format == "json")
+                {
+                    WriteGroupsToJsonFile(groups, writer);
+                }
+                else
+                {
+                    System.Console.Out.Write("Unrecognized format " + format);
+                }
+                writer.Close();
             }
-            writer.Close();
+           
+            System.Console.Out.Write("Complete! File created!");
+            //Файл генерируется в C:\Users\Александр
+            
 
+        }
+
+        static void WriteGroupsToExcelFile(List<GroupData> groups, string filename)
+        {
+            Excel.Application app = new Excel.Application(); //Вот этот код запускает Excel через Com интерфейс
+            app.Visible = true; //Делаем окно видимым
+            //Вот этот код запускает Excel через Com интерфейс
+
+            Excel.Workbook wb = app.Workbooks.Add();
+            Excel.Worksheet sheet = wb.ActiveSheet;
+            //sheet.Cells[1, 1] = "test";
+            int row = 1;
+            foreach (GroupData group in groups)
+            {
+                sheet.Cells[row, 1] = group.Name;
+                sheet.Cells[row, 2] = group.Header;
+                sheet.Cells[row, 3] = group.Footer;
+                row++;              
+            }
+            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), filename);
+            File.Delete(fullPath);
+            wb.SaveAs(fullPath); //Передаем полный путь к файлу, чтобы Excel сохранил туда куда нужно
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
         }
 
         static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
@@ -73,7 +114,12 @@ namespace addressbook_test_data_generators
         {
             new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
 
+        }
 
+        static void WriteGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+        {
+
+            writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
 
         }
 
--- addressbook2/Tests/GroupCreationTest.cs
+++ addressbook2/Tests/GroupCreationTest.cs
@@ -7,6 +7,8 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using System.Xml;
 using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace web_addressbook_test
 {
@@ -65,12 +67,46 @@ namespace web_addressbook_test
 
         }
 
+        public static IEnumerable<GroupData> GroupDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<GroupData>>(File.ReadAllText(@"groups.json"));
+        }
 
+        public static IEnumerable<GroupData> GroupDataFromExcelFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb = app.Workbooks.Open(Path.Combine(Directory.GetCurrentDirectory(), @"groups.xlsx"));//Открываем файл
+            Excel.Worksheet sheet = wb.ActiveSheet;//При открытии сразу попадаем на активную страницу (лист)
+            Excel.Range range = sheet.UsedRange;//Находим прямоугольник, который содержит какие-то данные
+
+            for (int i=1; i<=range.Rows.Count; i++)
+            {
+                groups.Add(new GroupData()
+                {
+                    Name = range.Cells[i,1].Value,
+                    Header = range.Cells[i, 2].Value,
+                    Footer = range.Cells[i, 3].Value,
+                });
+
+            }
+
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
+            return groups;
+
+
+        }
 
 
         //Для тестов из генерации так [Test, TestCaseSource("RandomGroupDataProvider")]
-        //Для тестов из файла так [Test, TestCaseSource("GroupDataFromFile")]
-        [Test, TestCaseSource("GroupDataFromXmlFile")]
+        //Для тестов из файла csv так [Test, TestCaseSource("GroupDataFromCsvFile")]
+        //Для тестов из файла json так [Test, TestCaseSource("GroupDataFromJsonFile")]
+        //Для тестов из файла xml так [Test, TestCaseSource("GroupDataFromXmlFile")]
+        //Для тестов из файла exlel так [Test, TestCaseSource("GroupDataFromExcelFile")]
+        //меняем тут название, копируем файл в наш проект, 
+        [Test, TestCaseSource("GroupDataFromExcelFile")]
         public void GroupCreationTest(GroupData group)
         {
             /*GroupData group = new GroupData("aaa");
